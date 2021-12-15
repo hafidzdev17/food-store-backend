0bb552eb2e52a1e28be854b158e5374287f0295b
hafet17 <do.crazy192@gmail.com>
2021-12-15 13:38:05 +0700

    add index database

---
+++ database/index.js
@@ -0,0 +1,27 @@
+// (1) import package mongoose
+const mongoose = require('mongoose');
+
+// (2) kita import konfigurasi terkait MongoDB dari `app/config.js`
+const {
+    dbHost,
+    dbName,
+    dbPort,
+    dbUser,
+    dbPass
+} = require('../app/config');
+
+// (3) connect ke MongoDB menggunakan konfigurasi yang telah kita import 
+mongoose
+    .connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+
+// (4) simpan koneksi dalam constant `db`
+const db =
+    mongoose.connection;;
+
+// (5) export `db` supaya bisa digunakan oleh file lain yang membutuhkan
+module.exports = db;
