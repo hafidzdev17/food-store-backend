08d390d742a8363666477d952b190036bc9d2cb3
hafet17 <do.crazy192@gmail.com>
2021-12-15 16:08:32 +0700

    feat(product): create product schema

---
+++ app/product/model.js
@@ -0,0 +1,35 @@
+// (1) import package `mongoose`
+const mongoose = require('mongoose');
+
+// (2) ambil module `model` dan `Schema` dari package `mongoose`
+const {
+    model,
+    Schema
+} = mongoose;
+
+
+const productSchema = Schema({
+
+    name: {
+        type: String,
+        minlength: [3, 'Panjang nama makanan minimal 3 karakter'],
+        required: [true, 'Nama makanan harus diisi']
+    },
+
+    description: {
+        type: String,
+        maxlength: [1000, 'Panjang deskripsi maksimal 1000 karakter']
+    },
+
+    price: {
+        type: Number,
+        default: 0
+    },
+
+    image_url: String,
+
+}, {
+    timestamps: true
+});
+
+module.exports = model('Product', productSchema);
