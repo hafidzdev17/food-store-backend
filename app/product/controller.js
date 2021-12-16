133c82fb290ddced8b2447ae9031e21260b5f9e0
hafet17 <do.crazy192@gmail.com>
2021-12-16 11:50:52 +0700

    feat(product): completed store product with upload image

--- app/product/controller.js
+++ app/product/controller.js
@@ -1,20 +1,95 @@
-// (1) import model `Product`
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('./model');
+const config = require('../config');
+
+
 
 async function store(req, res, next) {
 
-    let payload = req.body
+    try {
 
-    // (1) buat Product baru menggunakan data dari `payload`
-    let product = new Product(payload);
+        let payload = req.body;
 
-    // (2) simpan Product yang baru dibuat ke MongoDB
-    await product.save();
+        if (req.file) {
 
-    // (3) berikan response kepada client dengan mengembalikan product yang baru dibuat 
-    return res.json(product);
+            // menangkap lokasi sementara file yang diupload:
+            let tmp_path = req.file.path;
+
+            // menangkap ekstensi dari file yang diupload
+            let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+
+            // membangun nama file baru lengkap dengan ekstensi asli
+            let filename = req.file.filename + '.' + originalExt;
+
+            let target_path = path.resolve(config.rootPath, `public/uploads/product/${filename}`);
+
+            // (1) baca file yang masih di lokasi sementara 
+            const src = fs.createReadStream(tmp_path);
+
+            // (2) pindahkan file ke lokasi permanen
+            const dest = fs.createWriteStream(target_path);
+
+            // (3) mulai pindahkan file dari `src` ke `dest`
+            src.pipe(dest);
+
+            src.on('end', async () => {
+                try {
+
+                    let product = new Product({
+                        ...payload,
+                        image_url: filename
+                    })
+                    await product.save()
+                    return res.json(product);
+
+                } catch (err) {
+
+                    // (1) jika error, hapus file yang sudah terupload pada direktori
+                    fs.unlinkSync(target_path);
+
+                    // (2) cek apakah error disebabkan validasi MongoDB
+                    if (err && err.name === 'ValidationError') {
+                        return res.json({
+                            error: 1,
+                            message: err.message,
+                            fields: err.errors
+                        })
+                    }
+
+                    next(err);
+
+                }
+            });
+
+            src.on('error', async () => {
+                next(err);
+            });
+
+        } else {
+
+            let product = new Product(payload);
+            await product.save();
+            return res.json(product);
+
+        }
+    } catch (err) {
+
+        // ----- cek tipe error ---- //
+        if (err && err.name === 'ValidationError') {
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+
+        next(err);
+    }
 }
 
 module.exports = {
+    // index,
     store
 }
