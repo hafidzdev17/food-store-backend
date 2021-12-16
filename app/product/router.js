b1688a17d9184051ba32efe4506b8aeee1a287c7
hafet17 <do.crazy192@gmail.com>
2021-12-16 11:51:45 +0700

    feat(product): implement multer in router

--- app/product/router.js
+++ app/product/router.js
@@ -1,11 +1,11 @@
-// (1) import router dari express
 const router = require('express').Router();
+const multer = require('multer');
+const os = require('os');
 
-// (2) import product controller 
 const productController = require('./controller');
 
-// (3) pasangkan route endpoint dengan method `store`
-router.post('/products', productController.store);
+router.post('/products', multer({
+    dest: os.tmpdir()
+}).single('image'), productController.store);
 
-// (4) export router 
 module.exports = router;
